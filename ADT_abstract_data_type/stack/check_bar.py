b7c6003a576b974bc083c6cdec4e79360c679441
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2024-11-05 20:31:15 +0700

    nâng cấp check bar để nhận symbol nhiều hơn 1 char

--- ADT_abstract_data_type/stack/check_bar.py
+++ ADT_abstract_data_type/stack/check_bar.py
@@ -46,5 +46,46 @@ def check_bar_general(symbol_string):
     else:
         return False
 
-# Test the function
-print(check_bar_general('{{([][])}()}'))  # Should print True
+# if use want to handle /* and */ then first we need to set opens and closes to list.
+def matches_adv(top, symbol):
+    opens = ['(','{','[','/*']
+    closes = [')','}',']','*/']
+    return opens.index(top) == closes.index(symbol)
+
+def check_bar_adv(symbol_string):
+    s = Stack()
+    balance = True
+    index = 0
+    while index < len(symbol_string) and balance:
+        symbol = symbol_string[index]
+        # Check for multi-char first
+        if symbol == '/' and index + 1 < len(symbol_string) and symbol_string[index + 1] == '*':
+            s.push('/*')
+            index += 1
+        elif symbol == '*' and index + 1 < len(symbol_string) and symbol_string[index+1] == '/':
+            if s.is_empty() or s.pop() != '/*':
+                balance = False
+            index += 1
+
+        elif symbol in '({[':
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balance = False
+            else:
+                top = s.peek()
+                if not matches_adv(top, symbol):
+                    balance = False
+    if not s.is_empty() and not balance:
+        return False
+    return True
+
+if __name__ == "__main__":
+    s = '24TNT'
+    a = list(range(5))
+    w = s
+    # w =s
+    s = a[:3]
+    # s = [1,2,3]
+    print(s)
+    print(w)

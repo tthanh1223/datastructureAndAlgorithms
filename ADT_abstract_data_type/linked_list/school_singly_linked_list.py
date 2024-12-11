0c7219b7d3c7c7e5603d71990bdd0902aa313173
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2024-12-11 16:23:13 +0700

    sửa tên

--- ADT_abstract_data_type/linked_list/school_singly_linked_list.py
+++ ADT_abstract_data_type/linked_list/school_singly_linked_list.py
@@ -239,17 +239,6 @@ class SinglyLinkedList:
                 prev = current
             current = current.next
 
-class DoubleNode:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
 if __name__ == '__main__':
     lst = SinglyLinkedList()
     lst.add_head(10)

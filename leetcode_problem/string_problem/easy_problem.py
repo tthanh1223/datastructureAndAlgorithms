eb4098c9470d2416042793865dfefc0145b190cb
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2025-01-04 07:36:16 +0700

    add condition

--- leetcode_problem/string_problem/easy_problem.py
+++ leetcode_problem/string_problem/easy_problem.py
@@ -39,7 +39,7 @@ def is_subsequence(s: str, t: str) -> bool:
     if len(s) == 0: return True
     subsequence = 0
     for i in range(len(t)):
-        if s[subsequence] == t[i]:
+        if subsequence <= len(s) - 1 and s[subsequence] == t[i]:
             subsequence += 1
     return subsequence == len(s)
 
